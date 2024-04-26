1391dbda28a4c4453442fae206f38f15bd14f093
Ansari <ansari@mhmud.com.np>
2024-04-26 15:52:55 +0545

    Error in driver loading with getConnection is fixed

--- Lab2.java
+++ Lab2.java
@@ -17,7 +17,7 @@ public class Lab2 {
 			//1. Load the Driver
 			//Class.forName("org.postgresql.Driver");
 			//2. Establish the Connection
-			con=DriverManager.getConnection("jdbc:postgres://localhost:5432/java", "postgres", "admin");
+			con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/javaProgrmaming", "postgres", "admin");
 			//3. Prepare the SQL Statement
 			String sql ="select * from product";
 			//4. Create the JDBC Statement
@@ -26,9 +26,9 @@ public class Lab2 {
 			rs = st.executeQuery(sql);
 			//6. Process the Result
 			while( rs.next()) {
-				int id = rs.getInt(0);
-				String name = rs.getString(1);
-				int price = rs.getInt(2);
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				int price = rs.getInt(3);
 				System.out.println(id+"\t"+name+"\t"+price);
 			}
 		} catch (SQLException e) {
@@ -50,5 +50,4 @@ public class Lab2 {
 		}
 
 	}
-
 }
