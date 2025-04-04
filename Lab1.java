c7b9f83b08db8b8ad9695d2f65ddcd44045e4edb
self Mahmud <mahmud@codebase.com>
2025-04-04 18:07:03 +0545

    input fields are accepted dynamically from user and stored in the database

--- Lab1.java
+++ Lab1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package mahmud.com;
 
 import java.sql.*;
@@ -12,9 +11,10 @@ public class Lab1 {
         PreparedStatement pst = null;
         String jdbcURL = "jdbc:postgresql://localhost:5432/javaDB";
         String dbUser = "postgres";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the database Password: ");
-        String dbPassword = sc.nextLine();
+        String dbPassword = "admin";
+
+        UserInput userInput = new UserInput();
+
         try {
             //1. Load the Driver
             Class.forName("org.postgresql.Driver");
@@ -22,18 +22,16 @@ public class Lab1 {
             con=DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
             //3. Prepare the SQL Statement
             String createTable = "CREATE TABLE IF NOT EXISTS users(username varchar(25), password varchar(25))";
-            System.out.println("Enter the Username: ");
-            String name = sc.nextLine();
-            System.out.println("Enter the Password: ");
-            String password = sc.nextLine();
             String insertData ="INSERT INTO USERS VaLUES(?,?)";
             //4. Create the JDBC Statement
             st = con.createStatement();
             //5. Submit SQL Statement to Database using JDBC Statement
             st.executeUpdate(createTable);
             pst = con.prepareStatement(insertData);
-            pst.setString(1, name);
-            pst.setString(2, password);
+            userInput.input();  // Call to collect input
+            pst.setString(1, userInput.getUsername());
+            pst.setString(2, userInput.getPassword());
+
             int x = pst.executeUpdate();
             //6. Process the Result
             if( x ==1) {
@@ -46,7 +44,7 @@ public class Lab1 {
             e.printStackTrace();
         }
         //7. Close the Resources
-         finally {
+        finally {
             try {
                 if(st != null)
                     st.close();
@@ -60,52 +58,3 @@ public class Lab1 {
         }
     }
 }
-=======
-package com.mahmud.jlc;
-
-import java.sql.*;
-
-public class Lab1 {
-
-	public static void main(String[] args) throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-		Connection con = null;
-		ResultSet rs = null;
-		Statement st = null;
-		try {
-			//1. Load the Driver
-			Class.forName("org.postgresql.Driver");
-			//2. Establish the Connection
-			con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/javaProgrmaming", "postgres", "admin");
-			//3. Prepare the SQL Statement
-			String sql ="insert into product values(3,'Grapes',30)";
-			//4. Create the JDBC Statement
-			st = con.createStatement();
-			//5. Submit SQL Statement to Database using JDBC Statement
-			int x = st.executeUpdate(sql);
-			//6. Process the Result
-			if( x ==1) {
-				System.out.println("Inserted");
-			}else {
-				System.out.println("Not Inseted");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//7. Close the Resources
-		finally {
-			try {
-				if(st != null)
-					st.close();
-				if(rs != null)
-					rs.close();
-				if(con != null)
-					con.close();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-}
->>>>>>> 07cffc3533c1c1b0e35f915f526f1433a159857e
--- UserInput.java
+++ UserInput.java
@@ -1,2 +1,24 @@
-package mahmud.com;public class UserInput {
+package mahmud.com;
+
+import java.util.Scanner;
+
+public class UserInput {
+    private String username;
+    private String password;
+    Scanner sc = new Scanner(System.in);
+
+    public void input() {
+        System.out.println("Enter your username: ");
+        username = sc.nextLine();
+        System.out.println("Enter your password: ");
+        password = sc.nextLine();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
